89c49bb870fad065bfdb2864a5b23dc3fb00e7bf
Dawstin Waters <dawstinw4@gmail.com>
2024-07-11 04:20:52 -0700

    Updated myDie for the class

--- Dawstin - DieRoller/Program.cs
+++ Dawstin - DieRoller/Program.cs
@@ -4,4 +4,8 @@ using Dawstin___DieRoller;
 Die myDie = new Die(); // When the die is created, a random FaceValue should be set
 myDie.FaceValue = 6; // Setting face value shouldn't be allowed
 byte currentValue = myDie.FaceValue;
+myDie.Roll();
+myDie.Roll();
+myDie.Roll();
+
 Console.ReadKey();
