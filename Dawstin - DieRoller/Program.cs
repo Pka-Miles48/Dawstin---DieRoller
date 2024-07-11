b47abc2c1d2fd25dbe1af09ea1e82377df2e4539
Dawstin Waters <dawstinw4@gmail.com>
2024-07-10 22:17:55 -0700

    Created a code for DieRoller

---
+++ Dawstin - DieRoller/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using Dawstin___DieRoller;
+
+Die myDie = new Die(); // When the die is created, a random FaceValue should be set
+myDie.FaceValue = 6; // Setting face value shouldn't be allowed
+byte currentValue = myDie.FaceValue;
+Console.ReadKey();
