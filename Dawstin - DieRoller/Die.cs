28b75383c6c24741b3a372e503b4bbef2139e60f
Dawstin Waters <dawstinw4@gmail.com>
2024-07-11 05:19:25 -0700

    Refactored Roll method to take into account IsHeld

--- Dawstin - DieRoller/Die.cs
+++ Dawstin - DieRoller/Die.cs
@@ -6,6 +6,15 @@
     public class Die
     {
         /// <summary>
+        /// Creates the die and rolls it to start with
+        /// a random number
+        /// </summary>
+        public Die()
+        {
+            Roll();
+        }
+
+        /// <summary>
         /// The face up value of the die
         /// </summary>
         public byte FaceValue { get; set; }
@@ -17,18 +26,21 @@
 
         /// <summary>
         /// Rolls the die and sets the <see cref="FaceValue">
-        /// to the new number. Returns the new number
+        /// to the new number if the die isn't currently held.
+        /// Returns the <see cref="FaceValue">
         /// </summary>
-        /// <returns>Returns the new random number</returns>
+        /// <returns>Returns the <see cref="FaceValue"></returns>
         public byte Roll()
         {
-            // Generate random number
-            Random random = new Random();
-            byte newValue = (byte)random.Next(1, 7);
+            if (!IsHeld)
+            {          
+                // Generate random number
+                Random random = new Random();
+                byte newValue = (byte)random.Next(1, 7);
 
-            FaceValue = newValue;
-
-            return FaceValue:
+                FaceValue = newValue;
+            }
+            return FaceValue;
         }
     }
 }
--- Dawstin - DieRoller/Program.cs
+++ Dawstin - DieRoller/Program.cs
@@ -2,6 +2,5 @@
 using Dawstin___DieRoller;
 
 Die myDie = new Die(); // When the die is created, a random FaceValue should be set
-myDie.FaceValue = 6; // Setting face value shouldn't be allowed
 byte currentValue = myDie.FaceValue;
 Console.ReadKey();
